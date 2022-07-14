fae4c512beabf4da366c6760e9742da3b02e6fea
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-14 16:38:30 +0430

    HOS-29createReserveEndPoint:edit(reserve):export default function

--- pages/api/reserve.js
+++ pages/api/reserve.js
@@ -1,7 +1,7 @@
 import { reserveABed } from "../../controllers/reserve"
 import connectToDb from "../../utils/connectToDb"
 
-export async function handler(req, res){
+export default async function handler(req, res){
     await connectToDb()
     if(req.method === "POST")reserveABed(req, res)
 }
