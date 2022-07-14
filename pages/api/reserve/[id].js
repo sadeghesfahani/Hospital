1b413940e10cbe629f8bcd83c9b2126333981fd0
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-14 16:58:21 +0430

    HOS-editReserveEndpoint: edit reserve end point

---
+++ pages/api/reserve/[id].js
@@ -0,0 +1,7 @@
+import { reserveABed } from "../../../controllers/reserve"
+import connectToDb from "../../../utils/connectToDb"
+
+export default async function handler(req, res){
+    await connectToDb()
+    if(req.method === "POST")reserveABed(req, res)
+}
